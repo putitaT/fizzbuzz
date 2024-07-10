c80060fc33b716cfda003abcb26a088b501578c3
putitaT <putita.t@arise.tech>
2024-07-10 18:46:40 +0700

    test case 10 should return 'Buzz'

--- normalfizzbuzz/fizzbuzz.test.ts
+++ normalfizzbuzz/fizzbuzz.test.ts
@@ -46,4 +46,9 @@ describe('Fizz Buzz', () => {
       expect(result).toEqual("Fizz")
     })
 
+    it('should return Buzz for number = 10', () => {
+      const result = fizzbuzz(10)
+      expect(result).toEqual("Buzz")
+    })
+
 })
