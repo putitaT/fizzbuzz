03ef487889ced0787a914ed6989f11f941b20ed3
putitaT <putita.t@arise.tech>
2024-07-10 21:31:17 +0700

    prepare for round 2

--- normalfizzbuzz/fizzbuzz.ts
+++ normalfizzbuzz/fizzbuzz.ts
@@ -1,11 +1,1 @@
-export const fizzbuzz = (num: number): string => {
-    if (num == 15) {
-        return "FizzBuzz"
-    }else if (num % 3 == 0) {
-        return "Fizz"
-    } else if (num % 5 == 0) {
-        return 'Buzz'
-    } else {
-        return num.toString()
-    }
-}
+export const fizzbuzz = (num: number): string => ""
