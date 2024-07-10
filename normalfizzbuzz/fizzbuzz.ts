95ab1c1b774e91c983db84c00869e06f79a5d45a
putitaT <putita.t@arise.tech>
2024-07-10 21:42:24 +0700

    test case 6 should return 'Fizz'

--- normalfizzbuzz/fizzbuzz.ts
+++ normalfizzbuzz/fizzbuzz.ts
@@ -1,13 +1,9 @@
 export const fizzbuzz = (num: number): string => {
-    if (num ==1) {
-        return "1"
-    } else if (num == 2) {
-        return "2"
-    } else if (num == 3) {
+    if (num % 3 == 0) {
         return "Fizz"
-    } else if (num == 4) {
-        return "4"
-    } else {
+    } else if (num == 5) {
         return "Buzz"
+    } else {
+        return num.toString()
     }
 }
