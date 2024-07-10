421631fdda0e9206bb51ee7af43c5d822acf1ce0
putitaT <putita.t@arise.tech>
2024-07-10 22:46:47 +0700

    [FizzBuzzOneIf]: test case 3 should return 'Fizz'

--- fizzbuzzoneif/fizzbuzzOneIf.test.ts
+++ fizzbuzzoneif/fizzbuzzOneIf.test.ts
@@ -9,5 +9,10 @@ describe('Fizz Buzz with One If', () => {
     it('should return 2 for number = 2', () => {
         const result = fizzbuzzOneIf(2)
         expect(result).toEqual("2")
-      })
+    })
+
+    it('should return Fizz for number = 3', () => {
+        const result = fizzbuzzOneIf(3)
+        expect(result).toEqual("Fizz")
+    })
 })
