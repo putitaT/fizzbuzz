a64d39e8c10bc093c04fda6abd750553c8cca5b8
putitaT <putita.t@arise.tech>
2024-07-16 00:06:58 +0700

    [FizzBuzzOneIf]: test case 5 should return 'Buzz'

--- fizzbuzzoneif/fizzbuzzOneIf.ts
+++ fizzbuzzoneif/fizzbuzzOneIf.ts
@@ -1,6 +1,7 @@
 export const fizzbuzzOneIf = (num: number) => {
-    if (num === 3) {
-        return "Fizz"
+    if (num%3 === 0 || num%5 === 0) {
+        const text = ["Fizz", "", "Buzz"]
+        return text[num%3]
     } else {
         return num.toString()
     }
