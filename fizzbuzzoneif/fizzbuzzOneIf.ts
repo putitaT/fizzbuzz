0fd8dc2503affd726a6f508ffc886e5d749183cc
putitaT <putita.t@arise.tech>
2024-07-29 22:58:56 +0700

    [FizzBuzzOneIf]: prepare for round 3

--- fizzbuzzoneif/fizzbuzzOneIf.ts
+++ fizzbuzzoneif/fizzbuzzOneIf.ts
@@ -1,10 +1,1 @@
-export const fizzbuzzOneIf = (num: number) => {
-    let obj: { [key: number]: string } = { 3: "Fizz", 5: "Buzz" }; 
-    let out: string = '';
-    for (const [key, value] of Object.entries(obj)) { 
-        if (num % Number(key) === 0) { 
-            out += value; 
-        }
-    }
-    return out || num.toString();
-}
+export const fizzbuzzOneIf = (num: number) => ""
